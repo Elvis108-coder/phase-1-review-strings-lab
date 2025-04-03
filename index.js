cc5c78378b5e91ae6a83f55b28ed469135b74310
Elviskiprono <biggeelvo831@gmail.com>
2025-04-03 11:28:10 +0300

    strings lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = "Grace Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+const myString = "template literal";
+
+const myNumber = 10;
+
+const myBoolean = false;
